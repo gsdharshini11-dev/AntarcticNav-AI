d412b1585380907e5879c9fde36bc81c710af476
sharuamuamu-oss <sharuamuamu@gmail.com>
2026-09-13 18:40:58 +0530

    Use programmatic Node Vite build runner to bypass Vercel shell bin permissions

---
+++ build.js
@@ -0,0 +1,8 @@
+﻿import { build } from 'vite';
+
+build().then(() => {
+  console.log('AntarcticNav AI build complete!');
+}).catch((err) => {
+  console.error('Build failed:', err);
+  process.exit(1);
+});
